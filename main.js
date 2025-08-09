70e2f1b91425c8c54027c5ead993a6e82c267dc3
RomanForDev <romanfortinidev@gmail.com>
2025-08-09 15:53:15 -0300

    corregido un poco html con clases y id y lógica de JS para asignarles el tipo al ticket para la compra

--- main.js
+++ main.js
@@ -2,33 +2,34 @@ let ticketsCampo = 100;
 let ticketsCampoT = 100;
 let ticketsPlateaA = 50;
 let ticketsPlateaB = 50;
+const clase = 0;
 
 class TicketCampo {
-    constructor(id, precio, cantidad){
-        this.id = id;
+    constructor(precio, cantidad, claseTicket){
         this.precio = precio;
         this.cantidad = ticketsCampo;
+        this.claseTicket = clase;
     }}
 
 class TicketCampoT {
-    constructor(id, precio){
-        this.id = id;
+    constructor(precio, cantidad, claseTicket){
         this.precio = precio;
         this.cantidad = ticketsCampoT;
+        this.claseTicket = clase;
     }}
 
 class TicketPlateaA {
-        constructor(id, precio){
-        this.id = id;
+        constructor(precio, cantidad, claseTicket){
         this.precio = precio;
         this.cantidad = ticketsPlateaA;
+        this.claseTicket = clase;
 }}
 
 class TicketPlateaB {
-        constructor(id, precio){
-        this.id = id;
+        constructor(precio, cantidad, claseTicket){
         this.precio = precio;
         this.cantidad = ticketsPlateaB;
+        this.claseTicket = clase;
 }}
 
 class Customer {
@@ -47,46 +48,44 @@ class Customer {
 
 const customers = [];
 const cart = [];
-// let newTicket = 
+let claseDelTicket = 0;
 
-function addTicket(ticket) {
-    cart.push(ticket);
+let botonMain = document.querySelectorAll('.buttonMain');
+function determinarClase(){
+    botonMain.forEach(element => {
+        if (botonMain.id = "bone") {
+            let claseDelTicket = 1;
+        }else if(botonMain.id = "btwo"){
+            let claseDelTicket = 2;
+        }else if(botonMain.id = "bthr"){
+            let claseDelTicket = 3;
+        }else if(botonMain.id = "bfou"){
+            let claseDelTicket = 4;
+        }
+    });
+    switch(claseDelTicket) {
+        case 1: 
+        case 2:
+        case 3:
+        case 4:
+    }
 }
+determinarClase();
 
-function checkCustomer(){
-    nombre = document.getElementById('name'); //e.taget.value;
-    edad = document.getElementById('age');//e.taget.value;
-    email = document.getElementById('email');//e.taget.value; chequear
-    descuento = checkDiscount();
-    return customers.push(new Customer(nombre, edad, email, descuento));
-}
+// function addTicket(ticket) {
+//     cart.push(ticket);
+// }
 
-// let formulario = `<form action="customer">
-//         <label for="name">Nombre</label>
-//         <input type="text" id="name"> <br>
-//         <label for="age" id="age">Edad</label>
-//         <input type="text"> <br>
-//         <label for="email">Email</label>
-//         <input type="text" id="email"> <br>
-//         <input type="submit" value="Enviar">
-//         <input type="reset" value="Limpiar">
-//     </form>`;
+// function checkCustomer(){
+//     nombre = document.getElementById('name'); //e.taget.value;
+//     edad = document.getElementById('age');//e.taget.value;
+//     email = document.getElementById('email');//e.taget.value; chequear
+//     descuento = checkDiscount();
+//     return customers.push(new Customer(nombre, edad, email, descuento));
+// }
 
-// let nuevoFormulario = document.createElement('div');
-// nuevoFormulario.innerHTML = `<form action="customer">
-//         <label for="name">Nombre</label>
-//         <input type="text" id="name"> <br>
-//         <label for="age" id="age">Edad</label>
-//         <input type="text"> <br>
-//         <label for="email">Email</label>
-//         <input type="text" id="email"> <br>
-//         <input type="submit" value="Enviar">
-//         <input type="reset" value="Limpiar">
-//     </form>`;
-
-// document.body.appendChild(nuevoForm);
-let boton = document.getElementById("confirm");
-boton.addEventListener("click", function(){
+let botonConfirmar = document.getElementById("confirm");
+botonConfirmar.addEventListener("click", function(){
 let nuevoForm = document.createElement('section');
 nuevoForm.innerHTML = `<form action="customer">
     <label for="name">Nombre</label>
@@ -100,3 +99,8 @@ nuevoForm.innerHTML = `<form action="customer">
     </form>`;
     document.body.appendChild(nuevoForm)
     document.getElementById('confirm').disabled = true});
+
+// let nombreCliente = ; 
+// let edadCliente = ;
+// let emailCliente = ;
+
